08f891d428a0299d00d4ea80adc9b607261283fd
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-09-22 18:47:01 +0500

    Add project files.

---
+++ FilesDemo/Model2.cs
@@ -0,0 +1,15 @@
+﻿
+namespace FilesDemo
+{
+    public class Model2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public string Phone { get; set; }
+        public string website { get; set; }
+        public Company Company { get; set; }
+    }
+}
---
+++ FilesDemo/User.cs
@@ -0,0 +1,11 @@
+﻿namespace FilesDemo
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Book Book { get; set; }
+    }
+}
